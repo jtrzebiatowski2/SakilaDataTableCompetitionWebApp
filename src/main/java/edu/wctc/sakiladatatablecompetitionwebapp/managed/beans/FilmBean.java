453c2153e15a2b57d7ab99de5a46e7e3b675439b
J-Tron <J-Tron@J-Tron-PC>
2014-03-27 12:11:41 -0500

    Complex Data Table utilizing the filter capabilities, a footer with total number of films, a "sticky" filter menu bar, pagination, and sorting by column.

--- src/main/java/edu/wctc/sakiladatatablecompetitionwebapp/managed/beans/FilmBean.java
+++ src/main/java/edu/wctc/sakiladatatablecompetitionwebapp/managed/beans/FilmBean.java
@@ -1,13 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package edu.wctc.sakiladatatablecompetitionwebapp.managed.beans;
 
 import edu.wctc.sakiladatatablecompetitionwebapp.ejb.FilmFacade;
-import edu.wctc.sakiladatatablecompetitionwebapp.jsf.*;
 import edu.wctc.sakiladatatablecompetitionwebapp.entity.Film;
 import java.io.Serializable;
 import java.util.List;
@@ -19,31 +12,76 @@ import javax.inject.Named;
 /**
  *
  * @author J-Tron
+ * 
  */
-@Named
+@Named("filmBean")
 @SessionScoped
 public class FilmBean implements Serializable {
+
+    private Film current;
+    private List<Film> listOfFilms;
+    private int selectedItemIndex;
+    private Film selectedFilm;
+    private List<Film> filteredFilms;
+
     @Inject
-    private FilmFacade filmFacade;
-    private List<Film> filmList;
-    
-    public FilmBean(){
+    FilmFacade filmFacade;
+
+    public FilmBean() {
     }
-    
+
     @PostConstruct
-    private void init(){
-        getFilms();
-    }
-    private void getFilms(){
-        filmList= filmFacade.findAll();
+    public void init() {
+        listOfFilms = filmFacade.findAll();
     }
 
-    public List<Film> getFilmList() {
-        return filmList;
+    public Film getCurrent() {
+        return current;
     }
 
-    public void setFilmList(List<Film> filmList) {
-        this.filmList = filmList;
+    public void setCurrent(Film current) {
+        this.current = current;
     }
- 
+
+    public List<Film> getListOfFilms() {
+        return listOfFilms;
+    }
+
+    public void setListOfFilms(List<Film> listOfFilms) {
+        this.listOfFilms = listOfFilms;
+    }
+
+    public int getSelectedItemIndex() {
+        return selectedItemIndex;
+    }
+
+    public void setSelectedItemIndex(int selectedItemIndex) {
+        this.selectedItemIndex = selectedItemIndex;
+    }
+
+    public FilmFacade getFilmFacade() {
+        return filmFacade;
+    }
+
+    public void setFilmFacade(FilmFacade filmFacade) {
+        this.filmFacade = filmFacade;
+    }
+
+    public Film getSelectedFilm() {
+        return selectedFilm;
+    }
+
+    public void setSelectedFilm(Film selectedFilm) {
+        this.selectedFilm = selectedFilm;
+    }
+
+    public List<Film> getFilteredFilms() {
+        return filteredFilms;
+    }
+
+    public void setFilteredFilms(List<Film> filteredFilms) {
+        this.filteredFilms = filteredFilms;
+    }
+    
+
 }
